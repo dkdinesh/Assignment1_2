35d6c6e968e9dc5f387a850d4bf9b4c5a1a6a81e
DineshKumar <dkdinesh22@gmail.com>
2018-05-04 23:17:45 +0530

    Add files via upload

--- Assignment1_2.R
+++ Assignment1_2.R
@@ -1,30 +1,29 @@
 
+install.packages("readxl") #installing the package readxl used to read excel files
 
-require(readxl)
-install.packages("readxl")
+library(readxl) #loading files
 
-library(readxl)
+getwd() # checking current Dir
 
-getwd()
-
-setwd("C:/Users/appad500/Desktop/R")
+setwd("C:/Users/appad500/Desktop/R") # changing Dir as the path
 
 #1
 
 v<-c(2,5.5,6)
 t<-c(8,3,4)
-print(v%/%t)
-# > print(v%/%t)
+print(v%/%t) 
+
+#O/P
 # [1] 0 1 1
 
-#2
-read.file<-list.files(path = "C:/Users/appad500/Desktop/R", pattern = ".xlsx")
+#2  
+read.file<-list.files(path = "C:/Users/appad500/Desktop/R", pattern = ".xlsx") # fetching all the files
 read.file
-df.list<-lapply(read.file,read_excel)
+df.list<-lapply(read.file,read_excel) #reading the files
 df.list
 
 
 #3 
-read.flecsv<-list.files(pattern = "*.csv")
-read.flecsv
-dfcsv.list<- lapply(read.flecsv,read_csv)
+read.flecsv<-list.files(pattern = "*.csv") # fetching all CSV files
+read.flecsv 
+dfcsv.list<- lapply(read.flecsv,read_csv) # reading the files
